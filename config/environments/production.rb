6784e08bd93806042ff8409c8b32dd7c1fc2b1a2
Nikolay Efimov <nik.efimov@yahoo.ca>
2013-12-27 17:59:13 -0500

    Heroku Updates

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,9 +1,6 @@
 Vatatis::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
-  # Code is not reloaded between requests.
-  config.cache_classes = true
-
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both thread web servers
   # and those relying on copy on write to perform better.
@@ -20,17 +17,14 @@ Vatatis::Application.configure do
   # config.action_dispatch.rack_cache = true
 
   # Disable Rails's static asset server (Apache or nginx will already do this).
-  config.serve_static_assets = true
+  config.cache_classes        = true
+  config.serve_static_assets  = true
+  config.assets.compile       = true
+  config.assets.digest        = true
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
   # config.assets.css_compressor = :sass
-
-  # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
-
-  # Generate digests for assets URLs.
-  config.assets.digest = true
 
   # Version of your assets, change this if you want to expire all your assets.
   config.assets.version = '1.0'
