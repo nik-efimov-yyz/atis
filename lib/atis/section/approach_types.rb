5c958a2b01f28c2f8f00680b9aad0ad7498ff91a
Ki-ll <kirill.lofichenko@gmail.com>
2013-12-27 17:16:37 -0600

    updated title and approach message

--- lib/atis/section/approach_types.rb
+++ lib/atis/section/approach_types.rb
@@ -1,11 +1,11 @@
 class ATIS::Section::ApproachTypes < ATIS::Section::Base
 
   format :en do
-    block :expect
+    block :expect if message.approach_types.any?
     message.approach_types.each do |t|
       block t.downcase.to_sym
     end
-    block :approach
+    block :approach if message.approach_types.any?
   end
 
   format :ru do
